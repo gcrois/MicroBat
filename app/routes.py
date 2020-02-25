38923727e6818a3406404cb6de64ff5d4a5f80ca
Poochofpain <poochofpain92@gmail.com>
2020-02-24 23:07:31 -0500

    Added n00b html

--- app/routes.py
+++ app/routes.py
@@ -8,12 +8,32 @@ from app import app
 # execute when the url specified in the .routes() function
 # is evoked.
 
+# Right now this just creates a very simple html page.
 @app.route('/')
 @app.route('/home')
 def home():
-    message = "This is the homepage for 'insert catchy project title here'"
-    return message
+    return '''
+<html>
+    <head>
+        <title>Home Page</title>
+    </head>
+    <body>
+        <h1>Home Page for 'insert catchy project title here.'</h1>
+        <h2>We need to figure out if we're doing this all in HTML or JS and CSS too.</h2>
+    </body>
+</html>'''
 
+# Generic webpage for all users.
+# no clue how we'd make a dynamic page for an ambiguous user.
 @app.route('/users')
 def users():
-    return "This page is for users."
+    return '''
+<html>
+    <head>
+        <title>USER</title>
+    </head>
+    <body>
+        <h1>This is the webpage participants will see.</h1>
+        <h1>It needs to be ambigous so we can create it dynamically for each user.</h1>
+    </body>
+</html>'''
