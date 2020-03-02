df0e83769fbcddf59f3bd5807a1770fa61ed8391
Poochofpain <poochofpain92@gmail.com>
2020-03-02 00:04:16 -0500

    Added Flask Shell Functionality

--- server.py
+++ server.py
@@ -1,2 +1,12 @@
 # This file is anemic for now, but it'll get bigger.
-from app import app
+from app import app, dataBase
+from app.models import User, Answer
+
+
+# This allows us to open up a Python shell for testing
+# without having to do all those silly flask imports first.
+# simply type "flask shell" in the command line.
+
+@app.shell_context_processor
+def make_shell_context():
+    return{'db': dataBase, 'User': User, 'Answer': Answer}
