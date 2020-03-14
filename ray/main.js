02af23022b3cf56aa9028b5a404a83261dc7d29c
Gregory Croisdale <gc@utk.edu>
2020-03-13 23:24:12 -0400

    began constructing raytracing framework and canvas platform for all games

---
+++ ray/main.js
@@ -0,0 +1,25 @@
+function makeColor() {
+  var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+  for (var i = 0; i < imageData.width; i++) {
+    setPixel(imageData, i, 0, 255, 100, 0);
+  }
+  context.putImageData(imageData, 0, 0);
+}
+
+
+/* takes image, position, and color data and sets image pixel */
+function setPixel(data, x, y, r, g, b, a = 255) {
+  let loc = (x + y * data.width) * 4;
+  data.data[loc + 0] = r;
+  data.data[loc + 1] = g;
+  data.data[loc + 2] = b;
+  data.data[loc + 3] = a;
+}
+
+
+function init() {
+  canvas = document.getElementById('Canvas');
+  context = canvas.getContext('2d');
+  canvas.width = 500;
+  canvas.height = 500;
+}
